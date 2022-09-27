6f41a00d75c5c78e406ee5e46b1f4b8c63335d4c
goporo <goporolo1@gmail.com>
2022-09-27 21:45:03 +0700

    sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,16 @@
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
     return (
         <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-
+                <Sidebar />
+                <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+                    Copyright 2022 JSM Media
+                </Typography>
             </Box>
         </Stack>
     )
