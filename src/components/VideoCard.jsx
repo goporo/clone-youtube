cd486c2b81f7eb7f8d766a1c11846c42eda2a0ac
goporo <goporolo1@gmail.com>
2022-10-04 18:12:49 +0700

    videos components

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+    console.log(snippet)
+    return (
+        <Card>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <CardMedia
+                    image={snippet?.thumbnails?.high?.url}
+                    alt={snippet?.title}
+                    sx={{ width: 358, height: 180 }}
+                />
+            </Link>
+        </Card>
+    )
+}
+
+export default VideoCard
