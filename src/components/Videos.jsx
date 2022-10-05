cde2b3cf9fd0099bb57561c3c6880a94250ab420
goporo <goporolo1@gmail.com>
2022-10-05 21:01:01 +0700

    fix bug

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 import { VideoCard, ChannelCard } from './'
 
 const Videos = ({ videos, direction }) => {
+    // console.log(videos[5].snippet.thumbnails.default);
     if (!videos?.length) return 'Loading...'
 
     return (
         <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
             {videos.map((item, idx) => (
-                <Box key={idx}>
-                    {item.id.videoId && <VideoCard video={item} />}
-                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                item.snippet ?
+                    <Box key={idx}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && <ChannelCard channelDetail={item} />}
 
-                </Box>
+                    </Box>
+                    :
+                    <div />
             ))}
         </Stack>
     )
