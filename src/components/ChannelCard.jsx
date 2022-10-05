40bbc53b33594baa3da90b1cd37843bdcf49568e
goporo <goporolo1@gmail.com>
2022-10-05 18:23:49 +0700

    channel page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
     return (
         <Box
             sx={{
@@ -15,7 +15,8 @@ const ChannelCard = ({ channelDetail }) => {
                 justifyContent: 'center',
                 width: { xs: '356px', md: '320px' },
                 height: '326px',
-                margin: 'auto'
+                margin: 'auto',
+                marginTop,
             }}
         >
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
